d8345cb3d46ffc8160ca49a7a3c642d69a7e085d
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2022-09-20 11:56:29 +0530

    welcome

---
+++ welcome.c
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+void main()
+{//begining of the main()
+	//clrscr();
+	
+	printf("\n Welcome to ssi digital *");
+	printf("\n C is a high level programming language *");
+	printf("\n c is a structured programming language *");
+	getch();
+	
+}//ending of the main()
